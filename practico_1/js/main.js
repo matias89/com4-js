cdbce8fcb4e8af362b69c1cf954c511c40ff7f06
RafaEduMontero <rafaedumontero@gmail.com>
2020-07-15 13:45:26 -0300

    finished

--- practico_1/js/main.js
+++ practico_1/js/main.js
@@ -62,8 +62,6 @@
 
 };*/
 
-const error = '*Dato Requerido *';
-
 const procesarFormulario = event => {
     event.preventDefault(); // Necesario para que el submit no recargue la página y se pierdan los datos
 
@@ -86,17 +84,36 @@ const procesarFormulario = event => {
     const inputs = document.querySelectorAll('input');
     console.log(inputs);
     let countError = 0;
+
     inputs.forEach(element => {
         const { required, value, id } = element;
         if (required && value === '') {
-            document.querySelector(id).className = 'form-control is-invalid';
+            document.getElementById(id).className = 'form-control is-invalid';
             countError++;
         }
     });
 
     if (countError > 0) {
-        alert('No se han completado todos los campos requeridos. Los datos requeridos están indicados con *');
+        alert('No se han completado todos los campos requeridos. Los datos requeridos están indicados');
+    } else {
+        const myData = document.getElementById('my-data');
+        const formData = `
+        <h4><b>Nombre:</b> ${name.value}</h4>
+        <h4><b>Apellido:</b> ${surname.value}</h4>
+        <h4><b>Email:</b> ${email.value}</h4>
+        <h4><b>Fecha Nacimiento:</b> ${date.value}</h4>
+        <h4><b>Número Documento:</b> ${dni.value}</h4>
+        <h4><b>Provincia:</b> ${region.value}</h4>
+        <h4><b>Ciudad:</b> ${city.value}</h4>
+        <h4><b>Domicilio:</b> ${address.value}</h4>
+        <h4><b>Número:</b> ${number.value}</h4>
+        <h4><b>Dpto:</b> ${dpto.value}</h4>
+        <h4><b>Piso:</b> ${floor.value}</h4>
+    `;
+        myData.innerHTML = formData;
     }
+
+    return;
 
 
     /*const borrarFormulario = () => {
@@ -122,17 +139,26 @@ const procesarFormulario = event => {
         dpto.value = '';
         const floor = document.getElementById('floor');
         floor.value = '';
-    };
-
-    const cambiarClase = () => {
-        document.getElementById('name').className = 'form-control';
-        document.getElementById('surname').className = 'form-control';
-        document.getElementById('email').className = 'form-control';
-        document.getElementById('date').className = 'form-control';
-        document.getElementById('dni').className = 'form-control';
-        document.getElementById('region').className = 'form-control';
-        document.getElementById('city').className = 'form-control';
-        document.getElementById('address').className = 'form-control';
-        document.getElementById('number').className = 'form-control';
     };*/
+};
+
+const cambiarClase = () => {
+    const inputs = document.querySelectorAll('input');
+    console.log(inputs);
+    inputs.forEach(element => {
+        const { required, value, id } = element;
+        if (required && value !== '') {
+            document.getElementById(id).className = 'form-control';
+        }
+    });
+};
+
+const borrarFormulario = () => {
+    const inputs = document.querySelectorAll('input');
+    inputs.forEach(element => {
+        const { value, id } = element;
+        if (value !== '') {
+            document.getElementById(id).value = '';
+        }
+    });
 };
