c5fc690b1a528c85f4781d277fec435541a4de1c
Matias Lopez Klyver <lopezk_matias@hotmail.com>
2020-07-24 15:54:20 -0300

    practico2 solved

--- practico_2/index.js
+++ practico_2/index.js
@@ -1,8 +1,24 @@
 const URL = 'https://jsonplaceholder.typicode.com/comments';
 const APP = document.getElementById('app');
+const error = 'Ha ocurrido un error en tu solicitud.'
 
 const getComments = () => {
-    window.fetch(URL);
+    const comments = window.fetch(URL);
+    comments
+    .then(answer => {
+        answer.json()
+        .then(data => {
+            data.forEach(item => {
+                createComment(item);
+            });
+        })
+        .catch(error =>{
+            console.log('Error >>>', error);
+        })
+    })
+    .catch(error => {
+        console.log('Error >>>', error);
+    });
     // then y catch
     // en el then hacer el .json()
     // Recibir los datos y recorrerlos con forEach
@@ -18,8 +34,24 @@ const getComments = () => {
 const createComment = item => {
     const { postId, name, email, body, id } = item; // Item contiene la info recibida de la URL.
     const container = document.createElement('article'); // recordar article es como un div, pero semántico :)
-    const myId = document.createElement('h5');
+    container.className = 'card-body m-3 border border-dark bg-secondary text-white text-center rounded-lg'
+    const myId = document.createElement('h6');
+    myId.className = 'text-left'
     myId.innerText = id; // aqui he creado id, deberian crear los otros, usando <p>, <h3>, o lo que quieran
     // Crear los demas elementos para title, email y body.
     // hacer los appendChild correspondientes
+    const tittle = document.createElement ('h4');
+    tittle.className = 'text-uppercase text-white'
+    tittle.innerText = name;
+    const emails = document.createElement ('h6');
+    emails.className = 'text-white'
+    emails.innerText = email;
+    const bodys = document.createElement ('p');
+    bodys.className = 'text-left text-white font-italic'
+    bodys.innerText = body;
+    container.appendChild(myId);
+    container.appendChild(tittle);
+    container.appendChild(emails);
+    container.appendChild(bodys);
+    APP.appendChild(container);
 };
